98d910d37a9063ce7816d33bc931ebbbc75f40ad
Daisuke KANDA <dai1975@gmail.com>
2017-01-23 15:16:21 +0900

    add simplenote2

--- emacs/autoinsert-templates/python.py
+++ emacs/autoinsert-templates/python.py
@@ -1,13 +1,4 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-
-
-
-# Local Variables:
-# mode: python
-# coding: utf-8-unix
-# indent-tabs-mode: nil
-# c-basic-offset: 3
-# tab-width: 8
-# End:
+#if __name__ == '__main__':
