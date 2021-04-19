c5233d3d604cb6a187b21fc8f151e82a81859bd0
KANDA Daisuke <dai1975@gmail.com>
2021-04-19 09:36:11 +0900

    debug

--- bin/keccak.py
+++ bin/keccak.py
@@ -2,11 +2,13 @@
 # -*- coding: utf-8 -*-
 
 import sys
-import sha3
-
+#import sha3
+from Crypto.Hash import keccak
 
 def f(s):
-    k = sha3.keccak_256()
+    #k = sha3.keccak_256()
+    print(s)
+    k = keccak.new(digest_bits=256)
     k.update(s)
     print(k.hexdigest())
 
@@ -14,5 +16,5 @@ if __name__ == '__main__':
     if len(sys.argv) < 2:
         print("{} <input string>".format(sys.argv[0]))
         sys.exit(0)
-    f(sys.argv[0].encode("UTF-8"))
+    f(sys.argv[1].encode("UTF-8"))
 
